79812e95f0fd30fcff4b72d1f7b39b7cdb7f9361
DevMayani <106169881+DevMayani@users.noreply.github.com>
2023-10-25 01:03:51 +0100

    add

---
+++ my-app/src/pages/career.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '@/Components/Navbar/navbar'
+import Career from '../Components/Career/page'
+import Patner from '../Components/PatnerUs/page'
+import Footer from '@/Components/Footer/footer'
+
+const career = () => {
+  return (
+    <div>
+        <Navbar />
+        <Patner/>
+        <Footer />
+    </div>
+  )
+}
+
+export default career
