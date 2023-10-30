589fda8b9687e9d7dde51b916ad56c13effefd48
DevMayani <106169881+DevMayani@users.noreply.github.com>
2023-10-30 05:46:46 +0100

    add new code base

--- my-app/src/Components/Career/page.jsx
+++ my-app/src/Components/Career/page.jsx
@@ -3,20 +3,23 @@ import styles from './page.module.css'
 import Patner from '../PatnerUs/page'
 import Link from 'next/link'
 
-const page = () => {
+const Page = () => {
   return (
     <div className={styles.main} data-aos="flip-left">
       <p className={styles.label}>Career</p>
       <div>
-        <p className={styles.text}>Are you passionate about the real estate industry? Do you have what it takes to succeed in the enriching world of Jaswill Properties Limited, send your qualification/CV to info@jaswillproperties.com</p>
+        <p className={styles.text}>
+          Are you passionate about the real estate industry? Do you have what it takes to succeed in the enriching world of Jaswill Properties Limited, send your qualification/CV to{' '}
+          <a href="mailto:info@jaswillproperties.com">info@jaswillproperties.com</a>
+        </p>
         <div className={styles.buttoncont}>
           <Link href={`/career`}>
-        <button className={styles.button}>Join Us</button>
-        </Link> 
+            <button className={styles.button}>Join Us</button>
+          </Link>
         </div>
       </div>
     </div>
   )
 }
 
-export default page
+export default Page;
