5a7076c8c69c3ecad1d08bc9535c21251d563563
DevMayani <106169881+DevMayani@users.noreply.github.com>
2023-12-28 14:54:26 +0100

    add new

--- my-app/src/Components/DashboardComp/realEstate/Real_success/page.jsx
+++ my-app/src/Components/DashboardComp/realEstate/Real_success/page.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 import Navbar from '@/Components/Navbar/navbar'
+import DashboardHeader from '../../dashboardheader/dashboardheader'
+import Footer from '@/Components/Footer/footer'
 
 const page = () => {
   return (
-   <div>
+   <div className='text-sm bg-slate-100'>
+    {/* <DashboardHeader /> */}
     <Navbar />
-    <div className='flex items-center justify-center'>
+    
+    <div className='flex items-center justify-center h-[70vh]'>
  
-    <p className='bg-red-300 text-xl'>Click on Properties to check properties newly added Properties</p>
+    <p className='text-xl '>Click on Properties to check properties newly added.</p>
    
     </div>
+    <Footer />
    </div>
   )
 }
