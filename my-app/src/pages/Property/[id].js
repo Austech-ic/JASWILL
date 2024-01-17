2eb82bbdfce680bb9942e57d5e84a0cffded53fc
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-01-17 23:45:08 +0100

    add new

--- my-app/src/pages/Property/[id].js
+++ my-app/src/pages/Property/[id].js
@@ -1,9 +1,25 @@
 
-import React, {useEffect} from 'react'
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Loading from '../../Components/loading'; 
 import Indiv from '../../Components/PropertyComp/[category]/page'
 
 const PropertyIndiv = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an API call or data fetching
+    const fetchData = async () => {
+      // Your actual data fetching logic goes here
+      // For demonstration purposes, let's use a timeout
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    };
+
+    fetchData();
+  }, []);
+
     const router = useRouter();
     const { id } = router.query;
   
@@ -12,7 +28,15 @@ const PropertyIndiv = () => {
       return null;
     }
   return (
-    <Indiv id={id} />
+    <main className=' overflow-x-auto'>
+    {
+      loading ? (
+        <Loading />
+      ): (
+        <Indiv id={id} />
+      )
+    }
+   </main>
   )
 }
 
--- my-app/src/pages/Property/property.js
+++ my-app/src/pages/Property/property.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Loading from '../../Components/loading'; 
 import Property from '@/Components/PropertyComp/property'
 import Footer from '@/Components/Footer/footer'
 
 const Propertypage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an API call or data fetching
+    const fetchData = async () => {
+      // Your actual data fetching logic goes here
+      // For demonstration purposes, let's use a timeout
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <Property />
-      <Footer />
-    </div>
+    <main className=' overflow-x-auto'>
+    {
+      loading ? (
+        <Loading />
+      ) : (
+        <div>
+        <Property />
+        <Footer />
+      </div>
+      )
+    }
+   </main>
   )
 }
 
